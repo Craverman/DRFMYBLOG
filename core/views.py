c11c295941180048a3428b31c6f9f9ecae82d582
Владислав Захаренков <Craverman>
2022-08-03 13:20:59 +0300

    Creating pagination, tags and feedback

--- core/views.py
+++ core/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import PostSerializer
+from .serializers import PostSerializer, TagSerializer
 from .models import Post
 from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework import pagination
+from rest_framework import generics
+from taggit.models import Tag
+from .serializers import ContactSerailizer
+from django.core.mail import send_mail
+from rest_framework.views import APIView
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -11,5 +22,44 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination 
 
 
+class TagDetailView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    pagination_class = PageNumberSetPagination
+    permission_classes = [permissions.AllowAny]
+
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug'].lower()
+        tag = Tag.objects.get(slug=tag_slug)
+        return Post.objects.filter(tags=tag)
+
+
+class TagView(generics.ListAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class AsideView(generics.ListAPIView):
+    queryset = Post.objects.all().order_by('-id')[:5]
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class FeedBackView(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ContactSerailizer
+
+    def post(self, request, *args, **kwargs):
+        serializer_class = ContactSerailizer(data=request.data)
+        if serializer_class.is_valid():
+            data = serializer_class.validated_data
+            name = data.get('name')
+            from_email = data.get('email')
+            subject = data.get('subject')
+            message = data.get('message')
+            send_mail(f'От {name} | {subject}', message, from_email, ['CraVerka@googlemail.com'])
+            return Response({"success": "Sent"})
