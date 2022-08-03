fa5fdadd59f6763a4fd57ff56b99a4b44ca04608
Zakharenkov Vladislav <85833732+Craverman@users.noreply.github.com>
2022-08-03 16:33:49 +0300

    Finishing api (#3)
    Co-authored-by: Владислав Захаренков <Craverman>

--- core/models.py
+++ core/models.py
@@ -6,8 +6,6 @@ from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
 
 
-
-
 class Post(models.Model):
     h1 = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
@@ -21,3 +19,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_name')
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.text
